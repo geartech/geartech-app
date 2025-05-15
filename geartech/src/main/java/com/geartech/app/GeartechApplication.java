dad963af379f75fb79ac101fafe1fd4093be3f15
leandro-mbs <leandromarques_21@hotmail.com>
2025-05-14 23:01:49 -0300

    Primeiro Commit!

---
+++ geartech/src/main/java/com/geartech/app/GeartechApplication.java
@@ -0,0 +1,13 @@
+package com.geartech.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeartechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeartechApplication.class, args);
+	}
+
+}
---
+++ geartech/src/test/java/com/geartech/app/GeartechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geartech.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeartechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
